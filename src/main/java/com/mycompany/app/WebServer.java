62ff9fda4d53d92e7f2779865001ba0a1c34f4e7
EtienneGomez <egomezb1701@alumno.ipn.mx>
2023-12-29 16:55:53 -0600

    update

--- src/main/java/com/mycompany/app/WebServer.java
+++ src/main/java/com/mycompany/app/WebServer.java
@@ -1,53 +1,25 @@
-/*
- *  MIT License
- *
- *  Copyright (c) 2019 Michael Pogrebinsky - Distributed Systems & Cloud Computing with Java
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
-
 package com.mycompany.app;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
-import java.io.InputStream;  
 import java.util.StringTokenizer;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;   
-import com.fasterxml.jackson.databind.ObjectMapper;             
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;   
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class WebServer {
-   
     private static final String STATUS_ENDPOINT = "/status";
     private static final String HOME_PAGE_ENDPOINT = "/";
     private static final String HOME_PAGE_UI_ASSETS_BASE_DIR = "/ui_assets/";
     private static final String ENDPOINT_PROCESS = "/procesar_datos";
 
-    private final int port; 
-    private HttpServer server; 
+    private final int port;
+    private HttpServer server;
     private final ObjectMapper objectMapper;
 
     public WebServer(int port) {
@@ -64,35 +36,30 @@ public class WebServer {
             return;
         }
 
-        HttpContext statusContext = server.createContext(STATUS_ENDPOINT); 
+        HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
         HttpContext taskContext = server.createContext(ENDPOINT_PROCESS);
         HttpContext homePageContext = server.createContext(HOME_PAGE_ENDPOINT);
         statusContext.setHandler(this::handleStatusCheckRequest);
         taskContext.setHandler(this::handleTaskRequest);
         homePageContext.setHandler(this::handleRequestForAsset);
 
-
-
         server.setExecutor(Executors.newFixedThreadPool(8));
         server.start();
     }
 
     private void handleRequestForAsset(HttpExchange exchange) throws IOException {
-        System.out.println("Petición GET para asset recibida: " + exchange.getRequestURI().getPath());
-
         if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
             exchange.close();
             return;
         }
 
         byte[] response;
+        String asset = exchange.getRequestURI().getPath();
 
-        String asset = exchange.getRequestURI().getPath(); 
-
-        if (asset.equals(HOME_PAGE_ENDPOINT)) { 
+        if (asset.equals(HOME_PAGE_ENDPOINT)) {
             response = readUiAsset(HOME_PAGE_UI_ASSETS_BASE_DIR + "index.html");
         } else {
-            response = readUiAsset(asset); 
+            response = readUiAsset(asset);
         }
         addContentType(asset, exchange);
         sendResponse(response, exchange);
@@ -100,16 +67,11 @@ public class WebServer {
 
     private byte[] readUiAsset(String asset) throws IOException {
         InputStream assetStream = getClass().getResourceAsStream(asset);
-
-        if (assetStream == null) {
-            return new byte[]{};
-        }
-        return assetStream.readAllBytes(); 
+        return assetStream == null ? new byte[]{} : assetStream.readAllBytes();
     }
 
     private static void addContentType(String asset, HttpExchange exchange) {
-
-        String contentType = "text/html";  
+        String contentType = "text/html";
         if (asset.endsWith("js")) {
             contentType = "text/javascript";
         } else if (asset.endsWith("css")) {
@@ -119,28 +81,29 @@ public class WebServer {
     }
 
     private void handleTaskRequest(HttpExchange exchange) throws IOException {
-        System.out.println("Petición POST recibida en /procesar_datos");
-        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) { 
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
             exchange.close();
             return;
         }
 
         try {
-            FrontendSearchRequest frontendSearchRequest = objectMapper.readValue(exchange.getRequestBody().readAllBytes(), FrontendSearchRequest.class); 
+            FrontendSearchRequest frontendSearchRequest = objectMapper.readValue(exchange.getRequestBody().readAllBytes(), FrontendSearchRequest.class);
             String frase = frontendSearchRequest.getSearchQuery();
+            if (frase == null || frase.trim().isEmpty()) {
+                sendErrorResponse(exchange, "Consulta vacía o inválida");
+                return;
+            }
             StringTokenizer st = new StringTokenizer(frase);
             FrontendSearchResponse frontendSearchResponse = new FrontendSearchResponse(frase, st.countTokens());
             byte[] responseBytes = objectMapper.writeValueAsBytes(frontendSearchResponse);
             sendResponse(responseBytes, exchange);
-
         } catch (IOException e) {
+            sendErrorResponse(exchange, "Error al procesar la solicitud");
             e.printStackTrace();
-            return;
         }
     }
 
     private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
-        System.out.println("Petición GET recibida en /status");
         if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
             exchange.close();
             return;
@@ -151,13 +114,19 @@ public class WebServer {
     }
 
     private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
-        System.out.println("Enviando respuesta, tamaño: " + responseBytes.length + " bytes");
         exchange.sendResponseHeaders(200, responseBytes.length);
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(responseBytes);
         outputStream.flush();
         outputStream.close();
     }
+
+    private void sendErrorResponse(HttpExchange exchange, String errorMessage) throws IOException {
+        byte[] responseBytes = errorMessage.getBytes();
+        exchange.sendResponseHeaders(400, responseBytes.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        outputStream.close();
+    }
 }
-
-
