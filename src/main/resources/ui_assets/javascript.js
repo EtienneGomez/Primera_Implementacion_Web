7b2eec28b4a97e0bf96c016e245eb299824f1586
EtienneGomez <egomezb1701@alumno.ipn.mx>
2023-12-29 17:06:49 -0600

    update

--- src/main/resources/ui_assets/javascript.js
+++ src/main/resources/ui_assets/javascript.js
@@ -1,12 +1,19 @@
-fetch('/procesar_datos', {
-    method: 'POST',
-    body: JSON.stringify({ query: query }),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        displayResults(data);
+function sendQuery() {
+    var query = document.getElementById('queryInput').value;
+    fetch('/procesar_datos', {
+        method: 'POST',
+        body: JSON.stringify({ searchQuery: query }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            displayResults(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function displayResults(data) {
+    // Lógica para mostrar los resultados en la página
+}
